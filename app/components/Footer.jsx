cb710f54b530a83c47d443a118c24665483dadc8
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-10-19 14:25:07 +0530

    footer started

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,16 +9,166 @@ export default function Footer() {
             <div className="col-md-3">
               <Image
                 src="/img/white-logo.svg"
-                width={559}
-                height={395}
-                alt="contact image"
+                width={221}
+                height={92}
+                alt="footer logo"
               />
+              <p>
+                Empowering industries with cutting-edge technological solutions
+                and unwavering commitment to excellence, to enhance productivity
+                and drive success.
+              </p>
+              <div className="social">
+                <a href="#">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-twitter-x"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-facebook"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-instagram"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-youtube"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-whatsapp"></i>
+                </a>
+              </div>
+
+              <p>Select Language</p>
             </div>
             <div className="col-md-9">
               <div className="row">
-                <div className="col-md-4"></div>
-                <div className="col-md-4"></div>
-                <div className="col-md-4"></div>
+                <div className="col-md-4">
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="footer-links">
+                    <h2>Products</h2>
+                    <ul>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                      <li>
+                        <a href="#">PSA Nitrogen Generator Plant</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
