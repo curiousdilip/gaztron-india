f663cc1eb8044e732f0ab5d8de26b9763951e630
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-10-26 12:46:44 +0530

    added privacy and supply policy

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,128 @@
+import Image from "next/image";
+import ContactBox from "../components/ContactBox";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+import KeyProducts from "../components/KeyProducts";
+import Quote from "../components/Quote";
+import "./privacypolicy.scss";
+export default function SupplyPolicy() {
+  return (
+    <>
+      <Navbar />
+      <section id="supply-policy">
+        <div className="supply-box">
+          <Image
+            className="bottom-dots"
+            src="/img/privacy-icon.png"
+            width={112}
+            height={112}
+            alt="icon supply policy"
+          />
+          <h2>Supply Policy</h2>
+          <div className="item-container">
+            <h3>Gaztron Supply Policy</h3>
+            <p>
+              At Gaztron, we believe in delivering excellence through our
+              products and services to our valued customers. Our Supply Policy
+              is designed to ensure a seamless and efficient supply chain that
+              upholds the highest standards of quality, reliability, and
+              customer satisfaction. This policy outlines the key principles and
+              guidelines that govern our supply operations:
+            </p>
+            <ol>
+              <li>
+                <strong>Commitment to Quality:</strong> We are committed to
+                providing high-quality On-Site PSA Nitrogen (N2), Oxygen &
+                Hydrogen Gas Generator Plants to our customers. Our
+                manufacturing processes adhere to the highest industry
+                standards, and we continuously invest in research and
+                development to enhance the performance and efficiency of our
+                products.
+              </li>
+              <li>
+                <strong> Custom-Made and Standard Solutions:</strong> Gaztron
+                offers both custom-made and standard industrial solutions to
+                cater to the diverse requirements of our clients. Whether you
+                need a tailored gas generator plant or a standard solution, we
+                ensure that every product meets our strict quality criteria and
+                is optimized for energy efficiency.
+              </li>
+              <li>
+                <strong>Reliable and Timely Delivery:</strong> We understand the
+                importance of timely delivery for our customers' projects. Our
+                supply chain is streamlined and well-managed to ensure that
+                orders are processed efficiently and products are dispatched
+                promptly. We work closely with reputable logistics partners to
+                ensure that our products reach our customers on time, every
+                time.
+              </li>
+              <li>
+                <strong> Purity and Performance: </strong>
+                Gaztron takes pride in delivering PSA Gas Generator Plants that
+                guarantee the highest purity of nitrogen, oxygen, and hydrogen
+                gases. Our plants are designed to perform optimally, meeting the
+                specific needs of various applications across industries.
+              </li>
+              <li>
+                <strong> Eco-Friendly Approach:</strong> Environmental
+                sustainability is at the core of our business philosophy. We are
+                dedicated to providing eco-friendly gas generator solutions that
+                minimize energy consumption and reduce carbon emissions. By
+                choosing Gaztron's products, our customers contribute to a
+                greener and more sustainable future.
+              </li>
+              <li>
+                <strong> Quality Assurance:</strong> Our rigorous quality
+                assurance system ensures that each product undergoes thorough
+                testing and inspection before it leaves our facilities. This
+                commitment to quality assurance guarantees that our customers
+                receive reliable and durable gas generator plants.
+              </li>
+              <li>
+                <strong> Export Services:</strong> Gaztron serves customers not
+                only within India but also around the world. We extend our
+                expertise and services globally, enabling industries worldwide
+                to benefit from our cutting-edge gas generator solutions.
+              </li>
+              <li>
+                <strong>After-Sale Services:</strong> Our commitment to customer
+                satisfaction doesn't end with the delivery of the product. We
+                provide comprehensive after-sale services, including
+                installation support, maintenance, and technical assistance, to
+                ensure that our customers' gas generator plants operate smoothly
+                throughout their lifespan.
+              </li>
+              <li>
+                <strong> Transparent Communication: </strong>Open and
+                transparent communication is the foundation of our relationship
+                with our customers. We keep our clients informed about the
+                status of their orders, delivery timelines, and any potential
+                challenges, fostering a strong and trustworthy partnership.
+              </li>
+              <li>
+                <strong> Continuous Improvement:</strong> At Gaztron, we believe
+                in continuous improvement and strive to exceed customer
+                expectations at every stage. We welcome feedback from our
+                customers and use it to enhance our products, services, and
+                supply chain processes continually.
+              </li>
+            </ol>
+            <p>
+              By adhering to this Supply Policy, Gaztron aims to strengthen its
+              position as India's leading manufacturer of high-quality On-Site
+              PSA Nitrogen (N2), Oxygen & Hydrogen Gas Generator Plants. Our
+              unwavering commitment to customer satisfaction and eco-friendly
+              solutions drives us to excel in delivering reliable, efficient,
+              and sustainable gas generator solutions for our valued customers
+              worldwide.
+            </p>
+          </div>
+        </div>
+        <KeyProducts />
+        <Quote />
+        <ContactBox />
+      </section>
+      <Footer />
+    </>
+  );
+}
