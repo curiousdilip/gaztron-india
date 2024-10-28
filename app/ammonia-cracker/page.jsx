0cdfce78b2d116f6c57215fae506a4418eac8307
curiousdilip <curiousdilip@gmail.com>
2024-10-28 12:28:49 +0530

    ammonia cracker

---
+++ app/ammonia-cracker/page.jsx
@@ -0,0 +1,260 @@
+import Image from "next/image";
+import ContactBox from "../components/ContactBox";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+import Quote from "../components/Quote";
+import "./ammonia-cracker.scss"
+export default function AmmoniaCracker() {
+    return (
+        <>
+            <Navbar />
+            <section id="ammonia-cracker">
+                <div className="ammonia-box">
+                    <h2>Gaztron’s Ammonia Cracker</h2>
+                    <div className="item-container">
+                        <Image
+                            src="/img/about-us.png"
+                            width={1240}
+                            height={681}
+                            alt="PSA Nitrogen Gas Generator Plant"
+                        />
+                        <p>We are India’s leading manufacturer and exporter of cutting-edge Ammonia Cracking and Purification solutions. With our extensive product portfolio, we offer a comprehensive selection of models to meet a diverse range of needs. Our advanced designs deliver cracked Ammonia Gas with industry-leading low dew points of up to (-80°), and flexible flow rates ranging from 3Nm3/hr to 500Nm3/hr. Our team of skilled engineers ensures that the cracked gas is consistently available at optimal pressures between 1 kg/cm2 and 8 kg/cm2, enabling peak performance for your operations.</p>
+                        <p>At Gaztron, we offer a versatile range of Ammonia Cracker Units, with flow rates ranging from 3 Nm3/hr to 500 Nm3/hr and a dew point of (-80°). Our units operate within a pressure range of 1 Kg/cm2 and 8 Kg/cm2, providing optimal performance for a variety of industrial applications.</p>
+                        <p>Gaztron Ammonia Cracker is an essential component for a variety of industrial processes, including reducing furnace environments, heat treatment furnaces such as annealing, sintering, and galvanizing, as well as the bright annealing of both carbon and stainless steel. Our high-quality cracker units provide reliable and efficient performance to meet the demands of these critical applications.</p>
+                    </div>
+                </div>
+                <div className="application">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s Ammonia Cracker Applications</h2>
+                    <p>Our unwavering dedication to research and development has established us as frontrunners in ammonia cracker technologies, which find widespread applications in various industries worldwide. Our continuous focus on innovation has positioned us as industry leaders in meeting the demand for hydrogen or hydrogen-nitrogen mixtures in diverse industrial sectors such as:-</p>
+                    <div className="item-container">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={211}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Heat Treatment Furnaces</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="working">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>How It Works?</h2>
+                    <p>Our Ammonia Cracking units feature advanced Catalytic Hydrogen Gas Generators that utilize innovative cracking technology to dissociate Ammonia (NH3) into Hydrogen and Nitrogen, ensuring reliable and consistent hydrogen gas production.</p>
+                    <Image
+                    className="formula"
+                        src="/img/about-us.png"
+                        width={837}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <p>The feedstock for our process is refrigeration-grade Ammonia, obtained from cylinders or tanks, which yields a product gas containing 75% Hydrogen and 25% Nitrogen. To meet specific customer needs, the gas can be further purified using a Molecular Sieves unit to eliminate un-cracked Ammonia and moisture.</p>
+                </div>
+                <div className="models">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s Ammonia Cracker Models</h2>
+                    <p>Discover the power of our advanced ammonia cracker, meticulously engineered to cater to the unique needs of our clients. Gaztron Ammonia Cracker plays a crucial role in diverse industrial processes. Our high-quality cracker units are designed to deliver reliable and efficient performance, ensuring they meet the exacting demands of these critical applications.</p>
+                    <div className="products">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={203}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <div className="name">
+                                <h3>Ammonia Cracker Unit</h3>
+                                <div className="btns">
+                                    <a href="#">Learn More</a>
+                                    <a href="#">Request a Quote</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="specifications">
+                    <h2>Technical Specifications of Our Ammonia Cracker & Purifier Models</h2>
+                    <div className="details">
+                        <p>Our innovative ammonia cracker offers a cost-effective solution for cracked ammonia gas production. With advanced engineering and efficient design, it delivers cracked ammonia gas at a significantly reduced cost compared to alternatives, optimizing your production process.</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Technical Deatils</th>
+                                    <th scope="col">GAZ-NH3-05</th>
+                                    <th scope="col">GAZ- NH3 -10</th>
+                                    <th scope="col">GAZ- NH3-20</th>
+                                    <th scope="col">GAZ- NH3 -30</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Flow Range</th>
+                                    <td>0 - 5 NM3/HR</td>
+                                    <td>6 - 10 NM3/HR</td>
+                                    <td>11 - 20 NM3/HR</td>
+                                    <td>21 - 50 NM3/HR</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Cracked  Gas Purity</th>
+                                    <td>75% H2 & 25% N2</td>
+                                    <td>75% H2 & 25% N2</td>
+                                    <td>75% H2 & 25% N2</td>
+                                    <td>75% H2 & 25% N2</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Hydrogen - High Purity</th>
+                                    <td >99%</td>
+                                    <td >99%</td>
+                                    <td >99%</td>
+                                    <td >99%</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Dew Point</th>
+                                    <td >-80°C</td>
+                                    <td >-80°C</td>
+
+                                    <td >-80°C</td>
+                                    <td >-80°C</td>
+
+                                </tr>    <tr>
+                                    <th scope="row">Pressure</th>
+                                    <td >1 kg/cm2 - 5 kg/cm2</td>
+                                    <td >1 kg/cm2 - 5 kg/cm2</td>
+                                    <td >1 kg/cm2 - 5 kg/cm2</td>
+                                    <td >1 kg/cm2 - 5 kg/cm2</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="features">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>What sets our Ammonia Crackers apart from others</h2>
+                    <div className="item-container">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                    </div>
+                </div>
+                <Quote />
+                <ContactBox />
+            </section>
+            <Footer />
+        </>
+    )
+}
