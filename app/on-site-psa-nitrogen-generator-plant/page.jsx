0ed10f79d8c4d18d9a18452a6f1d900128a68c28
curiousdilip <curiousdilip@gmail.com>
2024-10-29 14:45:41 +0530

    starting onsite nitrogen generator page

---
+++ app/on-site-psa-nitrogen-generator-plant/page.jsx
@@ -0,0 +1,27 @@
+export default function OnSiteNitrogenPlant(){
+    return(
+        <>
+        <section id="onsite-nitrogen">
+            <div className="nitrogen-box">
+
+            </div>
+            <div className="specialisation">
+
+            </div>
+            <div className="models"></div>
+            <div className="specifications">
+
+            </div>
+            <div className="features">
+
+            </div>
+            <div className="payback-chart">
+
+            </div>
+            <div className="faq">
+                
+            </div>
+        </section>
+        </>
+    )
+}
