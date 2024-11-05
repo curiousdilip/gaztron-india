def850f1932949abb00b168ab845ab1a934ebf79
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-11-05 19:46:21 +0530

    industrial-air-dryers added

---
+++ app/industrial-air-dryers/page.jsx
@@ -0,0 +1,265 @@
+import Image from "next/image";
+import ContactBox from "../components/ContactBox";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+import Quote from "../components/Quote";
+import "./industrial-air-dryers.scss";
+export default function AmmoniaCracker() {
+  return (
+    <>
+      <Navbar />
+      <section id="industrial-air-dryers">
+        <div className="industrial-air-dryers-box">
+          <h2>Industrial Air Dryers</h2>
+          <div className="item-container">
+            <Image
+              src="/img/about-us.png"
+              width={1240}
+              height={681}
+              alt="PSA Nitrogen Gas Generator Plant"
+            />
+            <p>
+              Gaztron's industrial air dryers are integral components of the
+              industrial gas sector, serving as indispensable infrastructure for
+              efficient gas processing and storage. These dryers are
+              meticulously engineered to ensure the effective removal of
+              moisture and impurities from the gases, facilitating their safe
+              and reliable utilization. An efficient insulation system is
+              incorporated into these dryers to preserve the required low
+              temperatures, ensuring their optimal performance.
+            </p>
+            <p>
+              Gaztron's industrial air dryers are critical assets in the gas
+              industry, providing essential support for the processing and
+              storage of gases. These dryers are meticulously designed to meet
+              stringent requirements, ensuring the reliable and efficient
+              removal of moisture and impurities from the industrial gases. With
+              Gaztron's tailored equipment, cryogenic plants can optimize their
+              operations and achieve enhanced performance.
+            </p>
+          </div>
+        </div>
+        <div className="application">
+          <Image
+            src="/img/about-us.png"
+            width={112}
+            height={112}
+            alt="PSA Nitrogen Gas Generator Plant"
+          />
+          <h2>Gaztron’s Industrial Air Dryers Applications</h2>
+          <p>
+            Through unwavering dedication to innovation and advancement,
+            Gaztron's Industrial Air Dryers have emerged as the vanguard in
+            drying technology, serving a wide array of industries across the
+            globe. By continuously pushing the boundaries of research and
+            development, we have solidified our position as the foremost
+            provider of efficient and reliable air drying solutions, catering to
+            diverse industrial sectors worldwide.
+          </p>
+          <div className="item-container">
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={396}
+                height={211}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Heat Treatment Furnaces</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="types">
+          <h2>Types of Gaztron’s Air Dryers</h2>
+          <div className="item-container">
+            <div className="dryer">
+              <h3>1. Refrigerated Type Air Dryer</h3>
+              <p>
+                Gaztron’s refrigerated air dryer is the prevailing choice for
+                drying compressed air and utility gases in various industrial
+                applications, such as drying plant air, seal gas, hydrogen gas
+                for generator cooling, and nitrogen gas drying. It is widely
+                utilized due to its ability to maintain an acceptable dew point
+                of +2°C at line pressure, which corresponds to a dew point of
+                -22°C at atmospheric pressure.
+              </p>
+            </div>
+            <div className="dryer">
+              <h3>2. Heatless Type Air Dryer</h3>
+              <p>
+                Gaztron’s Our heatless dryer design incorporates two vessels
+                that are filled with desiccant, ensuring a continuous cycle that
+                produces dry air. In this process, the wet air enters the bottom
+                of one vessel and travels upwards through the desiccant bed,
+                where the moisture is adsorbed. The dry air exits from the top
+                of the vessel. Simultaneously, a small portion of the dry air is
+                directed downward through the desiccant bed in the second
+                vessel, which undergoes re-generation. The moisture-laden purge
+                air is then vented out to the atmosphere, resulting in a purge
+                loss equivalent to approximately 7.5% of the total air flow. At
+                regular intervals, the vessels automatically switch over,
+                ensuring a seamless transition and continuous availability of
+                dry air without any sudden fluctuations.
+              </p>
+            </div>
+            <div className="dryer">
+              <h3>3. Heat Reactivated Type Air Dryer</h3>
+              <p>
+                Heat reactivated dryers are employed when extremely low dew
+                points of -60°C or -80°C are required for compressed air. In
+                this particular design, the desiccant is regenerated at elevated
+                temperatures, accompanied by a small amount of dry air purge. By
+                utilizing dry air purging and thermal regeneration, the residual
+                moisture loading on the desiccant is minimized, resulting in
+                remarkably low dew points being achieved.
+              </p>
+            </div>
+            <div className="dryer">
+              <h3>4. Heat of Compression (HOC) Type Air Dryer</h3>
+              <p>
+                Gaztron's Heat of Compression (HOC) Type Air Dryer offers highly
+                efficient drying capabilities for compressed air systems. This
+                advanced technology ensures that the compressed air is dried to
+                an extremely low dew point, providing exceptional dryness. With
+                the HOC design, the heat generated during the compression
+                process is utilized for regeneration, eliminating the need for
+                external energy sources. This not only enhances energy
+                efficiency but also reduces operating costs.
+              </p>
+            </div>
+            <div className="dryer">
+              <h3>5. No Purge Loss Type Air Dryers</h3>
+              <p className="m-0">
+                Leveraging our extensive industry expertise, we provide a
+                diverse selection of No Purge Loss Type Air Dryers. These dryers
+                are specifically designed to eliminate water vapor from
+                compressed air without any loss of the compressed air itself.
+                Our range encompasses various capacities, pressure ratings, and
+                dew point options, catering to the specific needs of our
+                clients. To ensure optimal performance and flawless operation,
+                each dryer undergoes rigorous testing on well-defined
+                parameters. This commitment to quality allows us to deliver
+                reliable, high-performing dryers that meet the expectations of
+                our end users.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="features">
+          <Image
+            src="/img/about-us.png"
+            width={112}
+            height={112}
+            alt="PSA Nitrogen Gas Generator Plant"
+          />
+          <h2>What sets our Ammonia Crackers apart from others</h2>
+          <div className="item-container">
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>{" "}
+            <div className="box">
+              <Image
+                src="/img/about-us.png"
+                width={78}
+                height={78}
+                alt="PSA Nitrogen Gas Generator Plant"
+              />
+              <h3>Efficient & Energy-Saving</h3>
+            </div>
+          </div>
+        </div>
+        <Quote />
+        <ContactBox />
+      </section>
+      <Footer />
+    </>
+  );
+}
