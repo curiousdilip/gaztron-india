2487925fdf1ad566170141b2146414a47378a888
curiousdilip <curiousdilip@gmail.com>
2024-11-07 11:15:24 +0530

    psa-oxygen-generator-plant

---
+++ app/psa-oxygen-generator-plant/page.jsx
@@ -0,0 +1,337 @@
+import Image from "next/image";
+import ContactBox from "../components/ContactBox";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+import Quote from "../components/Quote";
+import "./psa-oxygen-generator-plant.scss"
+import FAQ from "../components/FAQs";
+export default function AmmoniaCracker() {
+    return (
+        <>
+            <Navbar />
+            <section id="psa-oxygen-generator-plant">
+                <div className="psa-oxygen-generator-plant-box">
+                    <h2>Gaztron’s PSA Oxygen Generator Plant </h2>
+                    <div className="item-container">
+                        <Image
+                            src="/img/about-us.png"
+                            width={1240}
+                            height={681}
+                            alt="PSA Nitrogen Gas Generator Plant"
+                        />
+                        <p>We are India’s leading manufacturer and supplier of state-of-the-art PSA Oxygen Generator Plants that deliver purities of up to 95% and delivery pressures of up to 5 kg/cm2, with a flow rate range of 5 Nm3/hr to 200 Nm3/hr. Our PSA Oxygen Generator Plant offer a cost-effective and efficient way of producing Oxygen on-site, providing industries & Hospitals with a reliable and flexible source of Oxygen supply.</p>
+                        <p>Gaztron's Oxygen Gas Generator Plants are designed to cater to a wide range of flow rates, starting from as low as 1 nm3/hr to 200 nm3/hr. Our generators can produce oxygen with a purity of up to 95% and a density of 5 kg/cm2g, with an impressively low dew point of (-) 40°C.</p>
+                        <p>At Gaztron, we understand the importance of a reliable source of Oxygen for various industries and hospital operations. That's why we offer advanced Oxygen Generators that are trusted for their quality and consistency. Our generators are <strong>used in various industries including glass manufacturing,
+                            metallurgy, chemical processing, water and wastewater treatment, metal gas welding, cutting, and brazing, as well as in fish farming and widely used in hospitals as on-site Oxygen Generators, ensuring a steady and safe supply of Oxygen for patients.</strong></p>
+                    </div>
+                </div>
+                <div className="specilisation">
+                <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s Specialisation</h2>
+                    <p>The demand for our Oxygen Generator Plants spans across different industries across the world. Our continuous focus on research and development has established us as industry leaders in the following domains:</p>
+                    <div className="products">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={203}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <div className="name">
+                                <h3>Oxygen Generator for Medical Purposes</h3>
+                                <p>Delivering dependable and secure on-site oxygen generation solutions for the healthcare industries.</p>
+                                <div className="btns">
+                                    <a href="#">Learn More</a>
+                                    <a href="#">Request a Quote</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="models">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s PSA Oxygen Gas Generator Plant Models</h2>
+                    <p>Experience the power of our advanced PSA Oxygen Generators, which are engineered to match the specific needs of our clients. With purities of up to 95% and delivery pressures of up to 5 kg/cm2, our generators offer a reliable source of Oxygen supply. Ranging from 5 Nm3/hr to 200 Nm3/hr, our flow rates provide flexibility, and our on-site manufacturing process ensures cost-effectiveness and efficiency.</p>
+                    <div className="products">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={203}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <div className="name">
+                                <h3>Oxygen Generator (GOX-M Model)</h3>
+                                <p>GOX-M Model is an innovative solution for generating high purity oxygen gas for medical purposes.</p>
+                                <div className="btns">
+                                    <a href="#">Learn More</a>
+                                    <a href="#">Request a Quote</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="specifications">
+                    <h2>Technical Specifications of Our PSA Oxygen Gas Generator Plant Models</h2>
+                    <div className="details">
+                        <p>Through our innovative engineering, our PSA oxygen generator plants offer the most economically viable solution for oxygen production, leveraging advanced technology to deliver oxygen at a significantly lower cost compared to other options.</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Gas Composition</th>
+                                    <th scope="col">Industrial Oxygen Generator</th>
+                                    <th scope="col">Medical Oxygen Generator</th>
+                                    <th scope="col">Oxygen Filling Station</th>
+                                    <th scope="col">PSA/VPSA Oxygen Generator</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Oxygen</th>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">02 Capacity (LPM)</th>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">02 Capacity (NM3/HR)</th>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nitrogen</th>
+                                    <td >7%</td>
+                                    <td >7%</td>
+                                    <td >7%</td>
+                                    <td >7%</td>
+
+                                </tr>    <tr>
+                                    <th scope="row">Dew Point</th>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Maintenance</th>
+                                    <td >EASY</td>
+                                    <td >EASY</td>
+                                    <td >SKILLED MANPOWER
+                                        REQUIRED</td>
+                                    <td >EASY</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Operation</th>
+                                    <td >EASY</td>
+                                    <td >EASY</td>
+                                    <td >SKILLED MANPOWER
+                                        REQUIRED</td>
+                                    <td >EASY</td>
+
+                                </tr> <tr>
+                                    <th scope="row">Required Utility</th>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Applications</th>
+                                    <td >Pharma, Food Packaging ,
+                                        Furnaces, Fire Control</td>
+                                    <td >Chemical, Pharma, Heat Treatment,
+                                        Electronic</td>
+                                    <td >Chemical & Metallurgical
+                                        Industries.</td>
+                                    <td >Electronics, Synthetic Fiber,
+                                        Pharma</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="capacity">
+                    <h2>Capacity of Gaztron’s Oxygen Generator Plant (GOX Model) In Terms of Cylinder Filled & Beds Catered</h2>
+                    <div className="details">
+                        <p>Gaztron's Gox model Oxygen Generator is a trusted and versatile piece of equipment that has demonstrated exceptional performance in producing oxygen gas. It has been a vital asset in India's fight against COVID-19, providing essential services to hospitals across the nation.</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Model</th>
+                                    <th scope="col">Capacity (NM3/HR)</th>
+                                    <th scope="col">Capacity (Liter/Min)</th>
+                                    <th scope="col">Equivalent Cylinder per day</th>
+                                    <th scope="col">Number of Beds Oxygen Plant can cater (3LPM)</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-5</th>
+                                    <td>5 NM3/HR</td>
+                                    <td>83</td>
+                                    <td>17</td>
+                                    <td>28</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-6</th>
+                                    <td>6 NM3/HR</td>
+                                    <td>100</td>
+                                    <td>25</td>
+                                    <td>42</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-10</th>
+                                    <td>10 NM3/HR</td>
+                                    <td>167</td>
+                                    <td>34</td>
+                                    <td>56</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-15</th>
+                                    <td>15 NM3/HR</td>
+                                    <td>250</td>
+                                    <td>51</td>
+                                    <td>83</td>
+
+                                </tr>    <tr>
+                                    <th scope="row">GAZ-GOX-20</th>
+                                    <td>20 NM3/HR</td>
+                                    <td>333</td>
+                                    <td>69</td>
+                                    <td>111</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-30</th>
+                                    <td>30 NM3/HR</td>
+                                    <td>500</td>
+                                    <td>103</td>
+                                    <td>167</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-40</th>
+                                    <td>40 NM3/HR</td>
+                                    <td>667</td>
+                                    <td>137</td>
+                                    <td>222</td>
+
+                                </tr> <tr>
+                                    <th scope="row">GAZ-GOX-50</th>
+                                    <td>50 NM3/HR</td>
+                                    <td>833</td>
+                                    <td>171</td>
+                                    <td>278</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">GAZ-GOX-60</th>
+                                    <td>60 NM3/HR</td>
+                                    <td>1000</td>
+                                    <td>206</td>
+                                    <td>333</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="features">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>What sets our Oxygen Generator apart from others</h2>
+                    <div className="item-container">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                    </div>
+                </div>
+                <FAQ />
+                <Quote />
+                <ContactBox />
+            </section>
+            <Footer />
+        </>
+    )
+}
