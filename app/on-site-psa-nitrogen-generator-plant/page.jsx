c5281505ffb96709f1b7e40872adc2018061fbba
curiousdilip <curiousdilip@gmail.com>
2024-11-07 11:54:56 +0530

    on-site-psa-nitrogen-generator-plant

--- app/on-site-psa-nitrogen-generator-plant/page.jsx
+++ app/on-site-psa-nitrogen-generator-plant/page.jsx
@@ -1,27 +1,263 @@
-export default function OnSiteNitrogenPlant(){
-    return(
+import Image from "next/image";
+import ContactBox from "../components/ContactBox";
+import Footer from "../components/Footer";
+import Navbar from "../components/Header";
+import Quote from "../components/Quote";
+import "./onsite-nitrogen.scss"
+import FAQ from "../components/FAQs";
+export default function AmmoniaCracker() {
+    return (
         <>
-        <section id="onsite-nitrogen">
-            <div className="nitrogen-box">
+            <Navbar />
+            <section id="onsite-nitrogen">
+                <div className="onsite-nitrogen-box">
+                    <h2>Gaztron’s On-Site PSA Nitrogen Generator Plant  </h2>
+                    <div className="item-container">
+                        <Image
+                            src="/img/about-us.png"
+                            width={1240}
+                            height={681}
+                            alt="PSA Nitrogen Gas Generator Plant"
+                        />
+                        <p>Gaztron Engineering Pvt Ltd is India’s leading designer, manufacturer, and exporter of top-quality On-Site PSA Nitrogen (N2) Gas Generator Plants. Our advanced technologies and unmatched expertise in the field enable us to offer reliable, cost-effective, and high-performance gas generation plant solutions to various industries worldwide.</p>
+                        <p>Our On-Site PSA Nitrogen (N2) Gas Generator Plants utilize the Pressure Swing Adsorption (PSA) technology to produce high-purity nitrogen gas with nitrogen purity ranging from 95% to 99.9998%. These generators are designed to cater to a diverse range of industrial applications, such as <strong>chemical, pharmaceuticals, electronics, food and beverage, oil & gas industry, inertization system and many more.</strong></p>
+                        <p>Furthermore, our On-Site Nitrogen (N2) Gas Generator Plants are equipped with advanced features, <strong>such as PLC control, touch screen HMI, dew point monitoring, oxygen analyzer, and automatic shutdown</strong> in case of any system malfunction or failure. We also provide gas discharge pressure options as per the specific needs of our clients, ranging from 5 Kg/cm2 to 200 Kg/cm2.</p>
+                    </div>
+                </div>
+                <div className="specilisation">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s Specialisation</h2>
+                    <p>Our On-Site PSA Nitrogen (N2) Gas Generator Plants are utilised in diverse industries worldwide, and our steadfast commitment to research and development has positioned us as industry pioneers in the following categories :</p>
+                    <div className="products">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={203}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <div className="name">
+                                <h3>Oxygen Generator for Medical Purposes</h3>
+                                <p>Delivering dependable and secure on-site oxygen generation solutions for the healthcare industries.</p>
+                                <div className="btns">
+                                    <a href="#">Learn More</a>
+                                    <a href="#">Request a Quote</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="models">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s PSA Nitrogen Gas Generator Plant Models</h2>
+                    <p>Our PSA Nitrogen (N2) Gas Generator Plants are available in different capacities, ranging from 10 Nm3/hr to 1000 Nm3/hr, and can be customized as per the specific requirements of our clients. We offer a broad range of nitrogen purity options to cater to different applications, including 95%, 97%, 99%, 99.5%, 99.9%, 99.99%, and 99.9998%.</p>
+                    <div className="products">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={396}
+                                height={203}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <div className="name">
+                                <h3>Oxygen Generator (GOX-M Model)</h3>
+                                <p>GOX-M Model is an innovative solution for generating high purity oxygen gas for medical purposes.</p>
+                                <div className="btns">
+                                    <a href="#">Learn More</a>
+                                    <a href="#">Request a Quote</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="specifications">
+                    <h2>Technical Specifications of Gaztron’s On-Site PSA Nitrogen (N2) Gas Generator Models</h2>
+                    <div className="details">
+                        <p>We've engineered our PSA nitrogen (n2) gas generator plants to provide the most cost-effective nitrogen production solution available, leveraging advanced technology to generate nitrogen gas at a fraction of the cost of competing options.</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Gas Composition</th>
+                                    <th scope="col">Industrial Oxygen Generator</th>
+                                    <th scope="col">Medical Oxygen Generator</th>
+                                    <th scope="col">Oxygen Filling Station</th>
+                                    <th scope="col">PSA/VPSA Oxygen Generator</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Oxygen</th>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
+                                    <td>93% ± 3%</td>
 
-            </div>
-            <div className="specialisation">
+                                </tr>
+                                <tr>
+                                    <th scope="row">02 Capacity (LPM)</th>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
+                                    <td>100-1500</td>
 
-            </div>
-            <div className="models"></div>
-            <div className="specifications">
+                                </tr>
+                                <tr>
+                                    <th scope="row">02 Capacity (NM3/HR)</th>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
+                                    <td >6-90</td>
 
-            </div>
-            <div className="features">
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nitrogen</th>
+                                    <td >7%</td>
+                                    <td >7%</td>
+                                    <td >7%</td>
+                                    <td >7%</td>
 
-            </div>
-            <div className="payback-chart">
+                                </tr>    <tr>
+                                    <th scope="row">Dew Point</th>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
+                                    <td >-40°C</td>
 
-            </div>
-            <div className="faq">
-                
-            </div>
-        </section>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Maintenance</th>
+                                    <td >EASY</td>
+                                    <td >EASY</td>
+                                    <td >SKILLED MANPOWER
+                                        REQUIRED</td>
+                                    <td >EASY</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Operation</th>
+                                    <td >EASY</td>
+                                    <td >EASY</td>
+                                    <td >SKILLED MANPOWER
+                                        REQUIRED</td>
+                                    <td >EASY</td>
+
+                                </tr> <tr>
+                                    <th scope="row">Required Utility</th>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+                                    <td >Dry Air, Cooling Water, Power</td>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">Applications</th>
+                                    <td >Pharma, Food Packaging ,
+                                        Furnaces, Fire Control</td>
+                                    <td >Chemical, Pharma, Heat Treatment,
+                                        Electronic</td>
+                                    <td >Chemical & Metallurgical
+                                        Industries.</td>
+                                    <td >Electronics, Synthetic Fiber,
+                                        Pharma</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="features">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>What sets our Oxygen Generator apart from others</h2>
+                    <div className="item-container">
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                        <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>   <div className="box">
+                            <Image
+                                src="/img/about-us.png"
+                                width={78}
+                                height={78}
+                                alt="PSA Nitrogen Gas Generator Plant"
+                            />
+                            <h3>Efficient & Energy-Saving</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="payback">
+                    <Image
+                        src="/img/about-us.png"
+                        width={112}
+                        height={112}
+                        alt="PSA Nitrogen Gas Generator Plant"
+                    />
+                    <h2>Gaztron’s PSA Nitrogen (N2) Gas Generator Payback</h2>
+                    <p>If your business relies on a steady supply of nitrogen, you're likely all too familiar with the challenges of renting and delivering bulky nitrogen cylinders. Not only do they take up valuable floor space and pose safety risks, but the associated costs, downtime, and contractual obligations can add up.</p>
+                    <p>While the initial investment may seem daunting, Gaztron's nitrogen generators typically pay for themselves in approximately 12 months, making them essentially free to operate after that point. Plus, you'll have the peace of mind of a reliable, cost-effective nitrogen supply that's under your control.</p>
+                    <div className="payback-period">
+                        
+                    </div>
+                </div>
+
+                <FAQ />
+                <Quote />
+                <ContactBox />
+            </section>
+            <Footer />
         </>
     )
 }
